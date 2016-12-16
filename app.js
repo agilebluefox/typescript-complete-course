aa25a7d46ad08045ee24e1789d6de251e1f91c3f
David Conner <agilebluefox@gmail.com>
2016-12-15 22:43:22 -0500

    Explore the various types in typescript
    A brief overview of the types and how typescript uses implicit and explicit type settings.

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('It works');
+// string
+var who = 'David';
+// number
+var age = 47;
+// boolean
+var hasAchesAndPains = true;
+// array
+var aches = ['sore legs', 'nerve impingement'];
+// tuple
+var achesAndPains = ['sore legs', 2];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 100] = "Blue";
+    Color[Color["Purple"] = 101] = "Purple";
+    Color[Color["Red"] = 2] = "Red";
+    Color[Color["Yellow"] = 3] = "Yellow"; // 3
+})(Color || (Color = {}));
+;
+var myColor = Color.Blue;
+console.log(myColor);
+// any
+var car = 'Honda';
+console.log(car);
+car = { brand: 'BMW', model: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
-console.log('It works');
+// string
+let who: string = 'David';
+
+// number
+let age: number = 47;
+
+// boolean
+let hasAchesAndPains: boolean = true;
+
+// array
+let aches: string[] = ['sore legs', 'nerve impingement'];
+
+// tuple
+let achesAndPains: [string, number] = ['sore legs', 2];
+
+// enum
+enum Color {
+    Gray, // 0
+    Green, // 1
+    Blue = 100, // 100
+    Purple, // 101
+    Red = 2, // 2
+    Yellow // 3
+};
+
+let myColor: Color = Color.Blue;
+console.log(myColor);
+
+// any
+let car: any = 'Honda';
+console.log(car);
+car = {brand: 'BMW', model: 3};
+console.log(car);

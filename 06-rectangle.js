8f0a6f5591126b5cc534aa34978f4b1170873f82
David Conner <agilebluefox@gmail.com>
2016-12-17 14:56:28 -0500

    Remove files from previous modules
    The typescript compiler does not realize the files in the directory are not related. I attempted to fix this issue by only compiing the app.ts file and outputting only the app.js file. However, as soon as the lessons moved into the module and namespace topic, additional problems began to surface such as using the outFile option with the CommonJS module option.
    
    To deal with this issue I'm simply going to work with the exercises as if the others never existed. Then when I push the changes to GitHub I can just refer to a particular commit to recall the lesson.

--- 06-rectangle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Geometry;
-(function (Geometry) {
-    function getRectangleArea(width, length) {
-        return 2 * width + 2 * length;
-    }
-    Geometry.getRectangleArea = getRectangleArea;
-})(Geometry || (Geometry = {}));
--- 06-rectangle.ts
+++
@@ -1,7 +0,0 @@
-// This file contains the rectangle functions for the Geometry namespace
-
-namespace Geometry {
-     export function getRectangleArea(width: number, length: number): number {
-        return 2 * width + 2 * length;
-    }
-}

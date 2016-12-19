9ce517602f46df93899b1e137c0b4b6fac96e396
David Conner <agilebluefox@gmail.com>
2016-12-18 20:17:42 -0500

    Remove directory of old files

--- geometry_functions/06-circle.js
+++
@@ -1,8 +0,0 @@
-// circle module
-"use strict";
-var PI = 3.14;
-// must export all functions to use them outside
-function getCircumference(diameter) {
-    return diameter * PI;
-}
-exports.getCircumference = getCircumference;
--- geometry_functions/06-rectangle.js
+++
@@ -1,7 +0,0 @@
-// rectangle module
-"use strict";
-function getRectangleArea(width, length) {
-    return 2 * width + 2 * length;
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = getRectangleArea;
